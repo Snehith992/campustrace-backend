d809590f3f3f4f8a52cfee7c500a2420d370e129
Snehith Nair <nairsnehith992@gmail.com>
2026-06-24 04:43:17 -0700

    Fix dotenv for Render deployment

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-require("dotenv").config({
-  path: "./models/cloud.env",
-});
+require("dotenv").config();
 
 const app = express();
 
